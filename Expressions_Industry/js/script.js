3e18585b89a49979f84f2f1d4f5260cc5252c3c2
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 14:42:09 -0400

    Added a new variable

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,6 +3,7 @@
 //Rounds Collected on average per day during 5 month training cycle
 var roundsCollectedDuringOneDay = [246, 95, 354, 222];
 var totalRoundsPerDay = roundsCollectedDuringOneDay[0] + roundsCollectedDuringOneDay[1] + roundsCollectedDuringOneDay[2] + roundsCollectedDuringOneDay[3];
+var trainingWeeksPerMonth = 4
 var trainingDaysPerWeek = 4
 var monthsInTrainingCycle = 5 
 var averageRoundsCollectedPerMonth = totalRoundsPerDay * trainingDaysPerWeek * trainingWeeksPerMonth ;
