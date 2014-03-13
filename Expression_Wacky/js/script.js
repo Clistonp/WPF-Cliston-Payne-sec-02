e7686f2782af6cba27f3ed3ee09f6148b8d6df42
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 16:54:49 -0400

    Expressions_Wacky Figuring out how to work the alert boxes

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -1,13 +1,12 @@
 //ClistonPayne 3/13/14 Expressions_Wacky Sec 02
 
 //Plumbing 
-prompt("You are a plumber and need to find the volume of a piece of pipe (Cylinder) for a job.")
+alert("You are a plumber and need to find the volume of a piece of pipe (Cylinder) for a job.")
 var height = prompt("How long is your piece of pipe in inches?");
 var diameter = prompt("What is the diameter of your pipe inches?");
-prompt("The volume of your piece of pipe is ")
 var radius = diameter / 2;
 var pie = 3.14;
 var volume = pie * (radius * radius) * height ;
-
+prompt("The volume of your piece of pipe is " + volume);
 
 alert(volume);
