d8445d3f7bb2959dc66bcd2dafd2a367185e8ea8
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 14:13:26 -0400

    Added array

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,6 @@
 //ClistonPayne 03/13/14 Expressions_Industry Sec 02
 
 //Rounds Collected on average per day during 5 month training cycle
+var roundsCollectedDuringOneDay[246, 95, 354, 222];
+var totalRoundsPerWeek = roundsCollectedDuringOneDay[0] + roundsCollectedDuringOneDay[1] + roundsCollectedDuringOneDay[2] + roundsCollectedDuringOneDay[3];
+console.log(totalRoundsPerWeek)
