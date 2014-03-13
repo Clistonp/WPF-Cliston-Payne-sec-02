477ba7a19fea4a78537e7270a0fa80ddaab9e79d
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 13:48:16 -0400

    Changed to my realistic values to see how much it actually cost me per year for dog food

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -1,7 +1,7 @@
 //Cliston Payne 03/13/14 Expressions_Personal sec 02
 
 //Budget for dog food for one year 
-var bagsOfDogFoodPerWeek = 1; //Place the value of how many bags of dog food you purchase perweeek
+var bagsOfDogFoodPerWeek = 2; //Place the value of how many bags of dog food you purchase perweeek
 var weeksInAYear = 52; // place the number of weeks per year or any value of weeks you want to budget for.
 var costPerBagOfFood = 35.96; // places the value of dog food perbag.
 var totalCostBudgeted = weeksInAYear * costPerBagOfFood * bagsOfDogFoodPerWeek; // formula for generating your cost of dog food for your givin time
