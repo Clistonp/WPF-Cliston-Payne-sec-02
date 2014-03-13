5c5c1d2ec187598ca01b21baee5a606c2c2e3e6a
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 15:21:16 -0400

    Expressions_Industry Had to change variable names
    Was not getting the results I was looking for

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,10 +1,6 @@
 //ClistonPayne 03/13/14 Expressions_Industry Sec 02
 
 //Rounds Collected on average per day during 5 month training cycle
-var roundsCollectedDuringOneDay = [246, 95, 354, 222];
-var totalRoundsPerDay = roundsCollectedDuringOneDay[0] + roundsCollectedDuringOneDay[1] + roundsCollectedDuringOneDay[2] + roundsCollectedDuringOneDay[3];
 var trainingWeeksPerMonth = 4
 var trainingDaysPerWeek = 4
 var monthsInTrainingCycle = 5 
-var averageRoundsCollectedPerMonth = totalRoundsPerDay * trainingDaysPerWeek * trainingWeeksPerMonth ;
-console.log(averageRoundsCollectedPerMonth)
