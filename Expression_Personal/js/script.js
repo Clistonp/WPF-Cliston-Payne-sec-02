99690c19203895a0f59e04106e0f3702a682207f
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 13:28:19 -0400

    Expressions_Personal Testing Code Changes

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -1,14 +1,14 @@
 //Cliston Payne 03/13/14 Expressions_Personal sec 02
 
 //Budget for dog food for one year 
-var bagsOfDogFoodPerWeek = // 1; //Place the value of how many bags of dog food you purchase perweeek
-var weeksInAYear = //52; // place the number of weeks per year or any value of weeks you want to budget for.
-var costPerBagOfFood = //35.96; // places the value of dog food perbag.
-var totalCostBudgeted = weeksInAYear * costPerBagOfFood * bagsOfDogFoodPerWeek; // formula for generating your cost of dog food for your givin time
+//var bagsOfDogFoodPerWeek = 1; //Place the value of how many bags of dog food you purchase perweeek
+//var weeksInAYear = 52; // place the number of weeks per year or any value of weeks you want to budget for.
+//var costPerBagOfFood = 35.96; // places the value of dog food perbag.
+//var totalCostBudgeted = weeksInAYear * costPerBagOfFood * bagsOfDogFoodPerWeek; // formula for generating your cost of dog food for your givin time
 
 //console.log(totalCostBudgeted) //Displays total Cost of food in the console of your browser // Disabled console.log for the prompt menu to budget your own values
 
- var bagsOfDogFoodPerWeek = prompt("How many bags of dog food do you go through weekly?");
- var weeksInAYear = prompt("How many weeks are in a year?");
-var costPerBagOfFood = prompt("How much is one bag of dog food?");
-alert (totalCostBudgeted)
+var bagsOfDogFoodPerWeek = prompt("How many bags of dog food do you go through weekly?");
+var weeksInAYear = prompt("How many weeks are in a year?");
+var costPerBagOfFood = prompt("How much does one bag of dog food cost?");
+alert (totalCostBudgeted =  bagsOfDogFoodPerWeek + weeksInAYear + costPerBagOfFood);
