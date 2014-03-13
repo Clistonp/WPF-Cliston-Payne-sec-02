ad3a8d88f9355a14af3f5e31580e17c4eff4a3c9
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 16:27:54 -0400

    Expressions_Wacky adding values

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -1,6 +1,9 @@
 //ClistonPayne 3/13/14 Expressions_Wacky Sec 02
 
 //Plumbing 
-prompt("You are a plumber and need to find the volume of a piece of pipe for a job.")
+prompt("You are a plumber and need to find the volume of a piece of pipe(Cylinder) for a job.")
 var height = prompt("How long is your piece of pipe?");
-console.log(height)
+var diameter = prompt("What is the diameter of your pipe?");
+var radius = diameter / 2;
+ 
+console.log(radius);
