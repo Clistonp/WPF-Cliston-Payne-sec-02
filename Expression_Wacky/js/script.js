47eb8a9b3a7097419f030c54591982f8cff319a8
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 16:46:56 -0400

    The change from inches

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -1,15 +1,11 @@
 //ClistonPayne 3/13/14 Expressions_Wacky Sec 02
 
 //Plumbing 
-prompt("You are a plumber and need to find the volume of a piece of pipe(Cylinder) for a job.")
-var height = prompt("How long is your piece of pipe?");
-var diameter = prompt("What is the diameter of your pipe?");
 prompt("You are a plumber and need to find the volume of a piece of pipe (Cylinder) for a job.")
 var height = prompt("How long is your piece of pipe in inches?");
 var diameter = prompt("What is the diameter of your pipe inches?");
 var radius = diameter / 2;
 var pie = 3.14;
-var volume = pie * (radius * radius) * height;
 var volume = pie * (radius * radius) * height prompt();
 
 
