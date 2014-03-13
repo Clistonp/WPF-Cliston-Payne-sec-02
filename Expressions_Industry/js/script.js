c49fc27c62439335b7fe010c63fc047125dbe1eb
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 15:32:44 -0400

    No comments on these files yet

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,12 +3,3 @@
 //Rounds Collected on average per day during 5 month training cycle
 var roundsCollectedDuringOneWeek = [246, 95, 354, 222]; // Each number represents 1 week of Round Collecting
 var totalRoundsPerMonth = roundsCollectedDuringOneWeek[0] + roundsCollectedDuringOneWeek[1] + roundsCollectedDuringOneWeek[2] + roundsCollectedDuringOneWeek[3];// Calculate how many rounds are collected per month
-var trainingWeeksPerMonth = 4
-var trainingDaysPerWeek = 4
-var monthsInTrainingCycle = 5 
-var totalRoundsPerDay = (roundsCollectedDuringOneWeek[0] + roundsCollectedDuringOneWeek[1] + roundsCollectedDuringOneWeek[2] + roundsCollectedDuringOneWeek[3]) / trainingDaysPerWeek;
-var totalTrainingDaysPerMonth = trainingDaysPerWeek * trainingWeeksPerMonth;
-var daysCollectingRoundsPerTrainingCycle = monthsInTrainingCycle * totalTrainingDaysPerMonth;
-var averageRoundsCollectedPerMonth = totalRoundsPerDay * trainingDaysPerWeek * trainingWeeksPerMonth;
-var roundsCollectedPerDayOnAverageDuringTrainingCycle = (averageRoundsCollectedPerMonth * monthsInTrainingCycle) / daysCollectingRoundsPerTrainingCycle;
-console.log(roundsCollectedPerDayOnAverageDuringTrainingCycle)
