09d455f4a591696abadc880f73701ec1fdfe0e14
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 15:22:22 -0400

    Expressions_Industry More Variable Changes
    Added changes as well as new Variables and switched a few around
    because I was not getting the correct answer.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,6 +1,14 @@
 //ClistonPayne 03/13/14 Expressions_Industry Sec 02
 
 //Rounds Collected on average per day during 5 month training cycle
+var roundsCollectedDuringOneWeek = [246, 95, 354, 222];
+var totalRoundsPerMonth = roundsCollectedDuringOneWeek[0] + roundsCollectedDuringOneWeek[1] + roundsCollectedDuringOneWeek[2] + roundsCollectedDuringOneWeek[3];
 var trainingWeeksPerMonth = 4
 var trainingDaysPerWeek = 4
 var monthsInTrainingCycle = 5 
+var totalRoundsPerDay = (roundsCollectedDuringOneWeek[0] + roundsCollectedDuringOneWeek[1] + roundsCollectedDuringOneWeek[2] + roundsCollectedDuringOneWeek[3]) / trainingDaysPerWeek;
+var totalTrainingDaysPerMonth = trainingDaysPerWeek * trainingWeeksPerMonth;
+var daysCollectingRoundsPerTrainingCycle = monthsInTrainingCycle * totalTrainingDaysPerMonth;
+var averageRoundsCollectedPerMonth = totalRoundsPerDay * trainingDaysPerWeek * trainingWeeksPerMonth;
+var roundsCollectedPerDayOnAverageDuringTrainingCycle = (averageRoundsCollectedPerMonth * monthsInTrainingCycle) / daysCollectingRoundsPerTrainingCycle;
+console.log(roundsCollectedPerDayOnAverageDuringTrainingCycle)
