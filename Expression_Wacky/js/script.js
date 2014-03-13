346714ee1e0ca1e38b14693cf97797db43715a57
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 17:13:12 -0400

    Added a final comment to my last line of code

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -8,5 +8,5 @@ var radius = diameter / 2; // formula to find the radius of a circle
 var pie = 3.14; // the representation for pie // pie is a repeating number but for the computers sake we are just using 3.14
 var volume = pie * (radius * radius) * height ; // formuala that will give us the volume of a cylinder
 alert("The volume of your piece of pipe is " + volume + " inches");// alert box that tells us the volume of our peice of pipe
-alert("Great job! Now get back to work.")
+alert("Great job! Now get back to work."); //Just being Wacky
 
