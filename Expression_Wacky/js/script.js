133762f507ebb09b65455de605dfef503af1b7f3
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 16:21:03 -0400

    Expressions_Wacky index.html

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -1,2 +1,6 @@
 //ClistonPayne 3/13/14 Expressions_Wacky Sec 02
 
+//Plumbing 
+prompt("You are a plumber and need to find the volume of a piece of pipe for a job.")
+var height = prompt("How long is your piece of pipe?");
+console.log(height)
