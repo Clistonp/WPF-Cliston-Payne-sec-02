2b58642c049bf6375887637e5cc4e133127b6d0c
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 15:23:57 -0400

    Added Comments Incomming

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,7 +1,7 @@
 //ClistonPayne 03/13/14 Expressions_Industry Sec 02
 
 //Rounds Collected on average per day during 5 month training cycle
-var roundsCollectedDuringOneWeek = [246, 95, 354, 222];
+var roundsCollectedDuringOneWeek = [246, 95, 354, 222]; // Each number represents 1 Day of Round Collecting
 var totalRoundsPerMonth = roundsCollectedDuringOneWeek[0] + roundsCollectedDuringOneWeek[1] + roundsCollectedDuringOneWeek[2] + roundsCollectedDuringOneWeek[3];
 var trainingWeeksPerMonth = 4
 var trainingDaysPerWeek = 4
