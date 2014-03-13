328804058730e597bd6384a7797aeef6a9ef53bb
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 14:41:13 -0400

    Expressions_Industry Changed Wording around because it wasn't giving me the correct answer i was looking for

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,4 +2,4 @@
 
 //Rounds Collected on average per day during 5 month training cycle
 var roundsCollectedDuringOneDay = [246, 95, 354, 222];
-var totalRoundsPerWeek = roundsCollectedDuringOneDay[0] + roundsCollectedDuringOneDay[1] + roundsCollectedDuringOneDay[2] + roundsCollectedDuringOneDay[3];
+var totalRoundsPerDay = roundsCollectedDuringOneDay[0] + roundsCollectedDuringOneDay[1] + roundsCollectedDuringOneDay[2] + roundsCollectedDuringOneDay[3];
