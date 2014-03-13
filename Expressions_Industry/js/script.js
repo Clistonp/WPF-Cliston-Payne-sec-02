745a051df2c67335e2f96464bd9b8258fb71571d
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 15:36:28 -0400

    More test code

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,8 +4,8 @@
 var roundsCollectedDuringOneWeek = [365, 789, 65, 125]; // Each number represents 1 week of Round Collecting
 var totalRoundsPerMonth = roundsCollectedDuringOneWeek[0] + roundsCollectedDuringOneWeek[1] + roundsCollectedDuringOneWeek[2] + roundsCollectedDuringOneWeek[3];// Calculate how many rounds are collected per month
 var trainingWeeksPerMonth = 4 //Number of weeks per training month
-var trainingDaysPerWeek = 4 //days per training week 
-var monthsInTrainingCycle = 5 // the number of weeks you are in the training cycle
+var trainingDaysPerWeek = 7 //days per training week 
+var monthsInTrainingCycle = 6 // the number of weeks you are in the training cycle
 var totalRoundsPerDay = (roundsCollectedDuringOneWeek[0] + roundsCollectedDuringOneWeek[1] + roundsCollectedDuringOneWeek[2] + roundsCollectedDuringOneWeek[3]) / trainingDaysPerWeek;// total rounds per day collected
 var totalTrainingDaysPerMonth = trainingDaysPerWeek * trainingWeeksPerMonth; // gives the total training days per month
 var daysCollectingRoundsPerTrainingCycle = monthsInTrainingCycle * totalTrainingDaysPerMonth; //gives the days you spend collecting rounds at range
