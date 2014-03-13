ace68133cf854f4f6b62bf8f7a886de7a1bea1cb
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 13:02:58 -0400

    Expressions_Personal Disabled console.log for testing code
    Added a semicolon to correct code

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -3,7 +3,5 @@
 //Budget for dog food for one year 
 var bagsOfDogFoodPerWeek = 1; //Place the value of how many bags of dog food you purchase perweeek
 var weeksInAYear = 52; // place the number of weeks per year or any value of weeks you want to budget for.
-var costPerBagOfFood = 35.96 // places the value of dog food perbag.
 var totalCostBudgeted = weeksInAYear * costPerBagOfFood * bagsOfDogFoodPerWeek; // formula for generating your cost of dog food for your givin time
 
-console.log(totalCostBudgeted) //Displays total Cost of food in the console of your browser
