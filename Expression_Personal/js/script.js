150935e398843ff8d7a653baea088db74b0a4228
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 12:53:23 -0400

    expression_Personal places a description of the code

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -6,6 +6,7 @@ var weeksInAYear = 52;
 var costPerBagOfFood = 35.96
 var totalCostBudgeted = weeksInAYear * costPerBagOfFood * bagsOfDogFoodPerWeek;
 console.log(totalCostBudgeted)var weeksInAYear = 52; // place the number of weeks per year or any value of weeks you want to budget for.
+var weeksInAYear = 52; // place the number of weeks per year or any value of weeks you want to budget for.
 var costPerBagOfFood = 35.96 // places the value of dog food perbag.
 var totalCostBudgeted = weeksInAYear * costPerBagOfFood * bagsOfDogFoodPerWeek; // formula for generating your cost of dog food for your givin time
 
