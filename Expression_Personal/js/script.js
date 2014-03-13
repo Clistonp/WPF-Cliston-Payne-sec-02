0119e1664c0db60acc8d768f57d6bfdd36eae9db
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 12:49:08 -0400

    Expression_Personal change 1

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -1,7 +1,6 @@
 //Cliston Payne 03/13/14 Expressions_Personal sec 02
 
 //Budget for dog food for one year 
-var bagsOfDogFoodPerWeek = 1;
 var bagsOfDogFoodPerWeek = 1; //Place the value of how many bags of dog food you purchase perweeek
 var weeksInAYear = 52;
 var costPerBagOfFood = 35.96
