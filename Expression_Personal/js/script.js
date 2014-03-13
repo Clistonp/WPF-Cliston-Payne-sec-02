301d0655106a13254c6ef0849d4fd383a6f1ebd2
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 12:48:32 -0400

    Expression_Personal First comment
    Placed a comment next to bags per week  also fixed an issue with my
    styles.css format.

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -2,6 +2,7 @@
 
 //Budget for dog food for one year 
 var bagsOfDogFoodPerWeek = 1;
+var bagsOfDogFoodPerWeek = 1; //Place the value of how many bags of dog food you purchase perweeek
 var weeksInAYear = 52;
 var costPerBagOfFood = 35.96
 var totalCostBudgeted = weeksInAYear * costPerBagOfFood * bagsOfDogFoodPerWeek;
