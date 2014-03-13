b0caf48586e6a1774f1c5f9a0881d0e7c6f1e493
Cliston Payne <Clistonp@yahoo.com>
2014-03-13 15:33:03 -0400

    Added the rest of my comments to the file

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,3 +3,12 @@
 //Rounds Collected on average per day during 5 month training cycle
 var roundsCollectedDuringOneWeek = [246, 95, 354, 222]; // Each number represents 1 week of Round Collecting
 var totalRoundsPerMonth = roundsCollectedDuringOneWeek[0] + roundsCollectedDuringOneWeek[1] + roundsCollectedDuringOneWeek[2] + roundsCollectedDuringOneWeek[3];// Calculate how many rounds are collected per month
+var trainingWeeksPerMonth = 4 //Number of weeks per training month
+var trainingDaysPerWeek = 4 //days per training week 
+var monthsInTrainingCycle = 5 // the number of weeks you are in the training cycle
+var totalRoundsPerDay = (roundsCollectedDuringOneWeek[0] + roundsCollectedDuringOneWeek[1] + roundsCollectedDuringOneWeek[2] + roundsCollectedDuringOneWeek[3]) / trainingDaysPerWeek;// total rounds per day collected
+var totalTrainingDaysPerMonth = trainingDaysPerWeek * trainingWeeksPerMonth; // gives the total training days per month
+var daysCollectingRoundsPerTrainingCycle = monthsInTrainingCycle * totalTrainingDaysPerMonth; //gives the days you spend collecting rounds at range
+var averageRoundsCollectedPerMonth = totalRoundsPerDay * trainingDaysPerWeek * trainingWeeksPerMonth;// gives the rounds collected per month
+var roundsCollectedPerDayOnAverageDuringTrainingCycle = (averageRoundsCollectedPerMonth * monthsInTrainingCycle) / daysCollectingRoundsPerTrainingCycle;// gives the user the amount of rounds collected per day during a training cycle
+console.log(roundsCollectedPerDayOnAverageDuringTrainingCycle)// shows the user in the console the results of there equation
