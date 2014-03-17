04cae3f4be76ce28d9acc0b8518dfe388d946ad8
Cliston Payne <Clistonp@yahoo.com>
2014-03-17 12:26:35 -0400

    Added comments and entered the if and else statemnt

--- Conditionals_WorkSheet/js/script.js
+++ Conditionals_WorkSheet/js/script.js
@@ -1,4 +1,15 @@
 //Cliston Payne Conditionals_WorkSheet Sec 02   03/17/14
 
 //Stuff your Face 1
-//Are you heavy enough
+//Are you heavy enough?
+var compsWeight = 435; // gives the competitors weight to determine if he is able to enter in the contest
+
+if(compsWeight >= 250){
+	//if statement that tells us how much the competitor must weigh to quilify for the contest
+	console.log("You are able to enter the Pie eating contest");
+
+}else{
+	//else statement that displays a text if the competitor is not heavy enough what he will be told
+	console.log("You are to small go to the little girls Pie easting contest");
+
+}
