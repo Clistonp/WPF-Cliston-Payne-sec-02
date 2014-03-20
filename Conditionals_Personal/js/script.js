6f640d74f5d213c0d10c2104ae8dd0455864f760
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 11:32:14 -0400

    OIL

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -6,3 +6,4 @@
 var = milesSinceLastOilChange (prompt("How many miles has it been since your last oil change"));
 var = whatKindOfOilDoYouUse (prompt("Do you use premium or regular oil in your car"));
 var premium = 5000;
+var regular = 3000:
