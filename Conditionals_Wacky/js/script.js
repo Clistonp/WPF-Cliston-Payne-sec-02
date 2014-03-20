f35a13d6919c8ae1e429c16dac99b8a9f5e3a071
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 16:22:16 -0400

    Added Confirm script to my variables

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,9 +3,23 @@
 //Conditionals Wacky
 //Ounces of Mountain Dew per Day
 
-var ouncesPerBottle = (prompt("How many ounces are in 1 container of Dew you drink?")) // this is the prompt that ask how many ouncesof dew are in your preferred container, IE bottles or cans 
-var drinksPerDay = (prompt("how many containers of dew do you drink a day?"))// how many of these containers do you drink a day 
+var ouncesPerBottle = (prompt("How many ounces are in 1 container of Dew you drink?"))// this is the prompt that ask how many ouncesof dew are in your preferred container, IE bottles or cans
+if	(confirm(ouncesPerBottle)){
+
+} 
+else{
+	(alert("Please enter the amount of ounces in 1 container of your favorite caffinated beverage!"));//if statement that displays a prompt
+}
+
+var drinksPerDay = (prompt("how many containers of dew do you drink a day?"))// how many of these containers do you drink a day
+if (confirm(drinksPerDay)){
+	
+} 
+else{
+	(alert("Please enter the amount of drinks you have per day."));
+}
 var ouncesPerDay = ouncesPerBottle * drinksPerDay; // calculates the ounces you drink of Mountain dew  day 
+
 
 if (ouncesPerDay >= 15 && ouncesPerDay <= 24){// if statement that displays a console log if you drink between 15 and 24oz's of beverage
 	console.log("Gotta Keep the Energy up.");
