47dfa9fb9e7694e524a06b5ab36ad115ec0c8718
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 15:45:53 -0400

    Added comments to my code

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,15 +3,15 @@
 //Conditionals Wacky
 //Ounces of Mountain Dew per Day
 
-var ouncesPerBottle = (prompt("How many ounces are in 1 container of Dew you drink?"))
-var drinksPerDay = (prompt("how many containers of dew do you drink a day?"))
-var ouncesPerDay = ouncesPerBottle * drinksPerDay;
+var ouncesPerBottle = (prompt("How many ounces are in 1 container of Dew you drink?")) // this is the prompt that ask how many ouncesof dew are in your preferred container, IE bottles or cans 
+var drinksPerDay = (prompt("how many containers of dew do you drink a day?"))// how many of these containers do you drink a day 
+var ouncesPerDay = ouncesPerBottle * drinksPerDay; // calculates the ounces you drink of Mountain dew  day 
 
-if (ouncesPerDay >= 15 && ouncesPerDay <= 24){
+if (ouncesPerDay >= 15 && ouncesPerDay <= 24){// if statement that displays a console log if you drink between 15 and 24oz's of beverage
 	console.log("Gotta Keep the Energy up.");
 }
 else if ((ouncesPerDay >= 25 && ouncesPerDay <= 48) || ( ouncesPerDay >= 96 && ouncesPerDay <= 120)){
-	console.log("Your getting there or you may have had just a little bit too much!");
+	console.log("Your getting there or you may have had just a little bit too much!");// else if statement that calculates displays if you drink between 25 and 48 oz's and 96 and 120 
 }
 else{
 	console.log("Now thats just the right amount of Dew for you!");
