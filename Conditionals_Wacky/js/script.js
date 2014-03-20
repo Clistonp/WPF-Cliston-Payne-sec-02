f8c5c70ff55cb02c40a6725303b464a65e57d012
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 15:28:49 -0400

    Putting together the variables with if and else statements

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -7,3 +7,9 @@ var ouncesPerBottle = (prompt("How many ounces are in 1 container of Dew you dri
 var drinksPerDay = (prompt("how many containers of dew do you drink a day?")
 var ouncesPerDay = ouncesPerBottle * drinksPerDay
 
+if (ouncesPerDay >= 15 && ouncesPerDay <= 24){
+	console.log("Gotta Keep the Energy up.");
+}
+else if ((ouncesPerDay >= 25 && ouncesPerDay <= 48) || ( ouncesPerDay >= 96 && ouncesPerDay <= 120){
+	console.log("your getting there or you may have had just a little bit to much")
+}
