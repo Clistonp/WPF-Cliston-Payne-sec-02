b56d008725b2f64c9afa82ac8b8774f54788a89c
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 14:11:51 -0400

    Added comments to my program

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,8 +3,8 @@
 //Conditionals Industry
 //Proper Rounds to load
 
-var time = (prompt("What time is it?"));
-var rounds;
+var time = (prompt("What time is it?"));// time represents military time 1800 - 23.59 is 6:00 pm - 11:59 pm
+var rounds; // this is the tyoes of rounds you will use depemnding on what time of day it is 
 
-rounds = ( time > 1800 ) ? "Tracer" : "Ball Point" ;
-console.log(rounds);
+rounds = ( time > 1800 ) ? "Tracer" : "Ball Point" ; // ternary that will tell us based on the time of day what rounds we will use
+console.log(rounds); // console log that will display the typres of rounds we need to load up depending on what the prompt and ternary decide
