bbf6cb72ddb31c00b809234b5e09b32dcae08ffd
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 13:06:45 -0400

    Added comments to my code

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,16 +3,16 @@
 //Conditionals_Personal
 //Is it time for an oil change?
 
-var milesSinceLastOilChange = (prompt("How many miles has it been since your last oil change?"));
-var whatKindOfOilDoYouUse = (prompt("Do you use premium or regular oil in your car?"));
-var premium = 5000;
-var regular = 3000;
+var milesSinceLastOilChange = (prompt("How many miles has it been since your last oil change?")); // This represents how many miles you have traveled in between oil changes.
+var whatKindOfOilDoYouUse = (prompt("Do you use premium or regular oil in your car?")); // this represents the kind of oil you use for simolicity i have chasen to only use 2 differant kinds regular and premium
+var premium = 5000; // this is the milage you can go saftley on premium
+var regular = 3000; // this represents the value you can travel saftley on regular oil
  
 if ((premium = whatKindOfOilDoYouUse && milesSinceLastOilChange >= premium) || (regular = whatKindOfOilDoYouUse && milesSinceLastOilChange >= regular)){
-	console.log("You need an oil change!");
+	console.log("You need an oil change!"); // If statement that combines my variables and ask the computer to distinguish between premium and regular oil and the milage inbetween oil changes.
 }else{
 	console.log("You have a few more miles to go!");
-}
+}// else statement that lets the user know if they need and oil change or not 
 
 
 
