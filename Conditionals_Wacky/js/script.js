75fc04a6a9a6210349f90c7e07cb42d32d0241e6
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 15:39:30 -0400

    fixing code to run properly

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -10,10 +10,9 @@ var ouncesPerDay = ouncesPerBottle * drinksPerDay;
 if (ouncesPerDay >= 15 && ouncesPerDay <= 24){
 	console.log("Gotta Keep the Energy up.");
 }
-	console.log("Your getting there or you may have had just a little bit to much!")
 else if ((ouncesPerDay >= 25 && ouncesPerDay <= 48) || ( ouncesPerDay >= 96 && ouncesPerDay <= 120)){
 	console.log("Your getting there or you may have had just a little bit too much!");
 }
 else{
-	console.log("Now thats just the right amount of Dew for you!")
+	console.log("Now thats just the right amount of Dew for you!");
 }
