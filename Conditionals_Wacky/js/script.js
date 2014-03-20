d7a334768f1eeffce32ad4c8e8826055e79614e6
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 15:17:39 -0400

    Variables

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,3 +6,4 @@
 var ouncesPerBottle = (prompt("How many ounces are in 1 container of Dew you drink?");
 var drinksPerDay = (prompt("how many containers of dew do you drink a day?")
 var ouncesPerDay = ouncesPerBottle * drinksPerDay
+
