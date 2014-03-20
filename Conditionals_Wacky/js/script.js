24f939257551b3d0410d243fdc2a2125279cae0f
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 15:17:26 -0400

    Ounces of Mountain Dew Per Day

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,8 @@
+// Cliston Payne Conditionals Wacky 3/20/14
+
+//Conditionals Wacky
+//Ounces of Mountain Dew per Day
+
+var ouncesPerBottle = (prompt("How many ounces are in 1 container of Dew you drink?");
+var drinksPerDay = (prompt("how many containers of dew do you drink a day?")
+var ouncesPerDay = ouncesPerBottle * drinksPerDay
