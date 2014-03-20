1d340edaf3dafb8fbddac6476d525b99a1f886d3
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 15:39:00 -0400

    Correcting code to make the formulas work correctly

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,15 +3,16 @@
 //Conditionals Wacky
 //Ounces of Mountain Dew per Day
 
-var ouncesPerBottle = (prompt("How many ounces are in 1 container of Dew you drink?");
-var drinksPerDay = (prompt("how many containers of dew do you drink a day?")
-var ouncesPerDay = ouncesPerBottle * drinksPerDay
+var ouncesPerBottle = (prompt("How many ounces are in 1 container of Dew you drink?"))
+var drinksPerDay = (prompt("how many containers of dew do you drink a day?"))
+var ouncesPerDay = ouncesPerBottle * drinksPerDay;
 
 if (ouncesPerDay >= 15 && ouncesPerDay <= 24){
 	console.log("Gotta Keep the Energy up.");
 }
-else if ((ouncesPerDay >= 25 && ouncesPerDay <= 48) || ( ouncesPerDay >= 96 && ouncesPerDay <= 120){
 	console.log("Your getting there or you may have had just a little bit to much!")
+else if ((ouncesPerDay >= 25 && ouncesPerDay <= 48) || ( ouncesPerDay >= 96 && ouncesPerDay <= 120)){
+	console.log("Your getting there or you may have had just a little bit too much!");
 }
 else{
 	console.log("Now thats just the right amount of Dew for you!")
