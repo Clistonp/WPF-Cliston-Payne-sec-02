c4cf3432d02d85200761894bd48ecb046eccb504
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 11:29:00 -0400

    Naming Personal Assignment

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,8 @@
 // Cliston Payne Conditionals_Personal 3/20/14 
+
 //Conditionals_Personal
 //Is it time for an oil change?
+
+var = milesSinceLastOilChange (prompt("How many miles has it been since your last oil change"));
+var = whatKindOfOilDoYouUse (prompt("Do you use premium or regular oil in your car"));
+var premium = 5000;
