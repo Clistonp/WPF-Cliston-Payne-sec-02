4124b44cca01820f8f752819b5cf64f1cb2053df
Cliston Payne <Clistonp@yahoo.com>
2014-03-20 11:40:34 -0400

    Miles to go

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,4 +8,5 @@ var = whatKindOfOilDoYouUse (prompt("Do you use premium or regular oil in your c
 var premium = 5000;
 var regular = 3000:
 var safeMilesBetweenOilChanges = whatKindOfOilDoYouUse 
-if (safeMilesBetweenOilChanges >= milesSinceLastOilChange)
+if (safeMilesBetweenOilChanges >= milesSinceLastOilChange){
+	console.log("You have a few more miles to go before you need to change your oil.")
