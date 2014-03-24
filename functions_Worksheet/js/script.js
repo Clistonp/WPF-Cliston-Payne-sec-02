29e479d132531df9326bbd78d602a3950da5b70c
Cliston Payne <Clistonp@yahoo.com>
2014-03-24 11:31:15 -0400

    function that will repeatedly run my code

--- functions_Worksheet/js/script.js
+++ functions_Worksheet/js/script.js
@@ -6,7 +6,7 @@ var pie = 3.14; // this is the value of pie
 var times = 2; // multiplied by to get circumference
 var circumference = calcCircumference(times, pie, radius); // variable that stores the value of the circumference
 console.log("The Circumference of the circle is " + circumference)// displays the answer to the circumference by using ghe console log
-function calcCircumference(times, pie, radius)
+function calcCircumference(times, pie, radius)// creates a function that will run the calculation
 {
 	return times *pie *radius;
 }
