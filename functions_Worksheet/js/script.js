63cbc80b5ce152320e6349954e5decaae4e8d042
Cliston Payne <Clistonp@yahoo.com>
2014-03-24 11:26:11 -0400

    fixed the code to work in console.log

--- functions_Worksheet/js/script.js
+++ functions_Worksheet/js/script.js
@@ -6,7 +6,7 @@ var pie = 3.14;
 var times = 2;
 var circumference = calcCircumference(times, pie, radius);
 console.log("The Circumference of the circle is " + circumference)
-function calcCircumference(times, pie, radius):Number
+function calcCircumference(times, pie, radius)
 {
 	return times *pie *radius;
 }
