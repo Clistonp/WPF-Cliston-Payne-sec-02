1ba8ab23dfe8687b2be12b6b13031ec9642339d6
Cliston Payne <Clistonp@yahoo.com>
2014-03-24 11:27:53 -0400

    the number that pie and radius have to be multiplied by

--- functions_Worksheet/js/script.js
+++ functions_Worksheet/js/script.js
@@ -3,7 +3,7 @@
 //Calculate the Circumference of a Circle
 var radius = 15;// this is the radius of my circle
 var pie = 3.14; // this is the value of pie
-var times = 2;
+var times = 2; // multiplied by to get circumference
 var circumference = calcCircumference(times, pie, radius);
 console.log("The Circumference of the circle is " + circumference)
 function calcCircumference(times, pie, radius)
