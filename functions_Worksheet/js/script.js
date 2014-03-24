a067ad25bdf3cc73449ba6ef62ef7593c07c2baa
Cliston Payne <Clistonp@yahoo.com>
2014-03-24 11:30:10 -0400

    console.log comment

--- functions_Worksheet/js/script.js
+++ functions_Worksheet/js/script.js
@@ -5,7 +5,7 @@ var radius = 15;// this is the radius of my circle
 var pie = 3.14; // this is the value of pie
 var times = 2; // multiplied by to get circumference
 var circumference = calcCircumference(times, pie, radius); // variable that stores the value of the circumference
-console.log("The Circumference of the circle is " + circumference)
+console.log("The Circumference of the circle is " + circumference)// displays the answer to the circumference by using ghe console log
 function calcCircumference(times, pie, radius)
 {
 	return times *pie *radius;
