067197e00450b1a914f3c649b3af29098e178948
Cliston Payne <Clistonp@yahoo.com>
2014-03-24 11:26:56 -0400

    added a comment for radius

--- functions_Worksheet/js/script.js
+++ functions_Worksheet/js/script.js
@@ -1,7 +1,7 @@
 //Cliston Payne functions_Worksheet 3/24/14
 
 //Calculate the Circumference of a Circle
-var radius = 15;
+var radius = 15;// this is the radius of my circle
 var pie = 3.14;
 var times = 2;
 var circumference = calcCircumference(times, pie, radius);
@@ -10,3 +10,5 @@ function calcCircumference(times, pie, radius)
 {
 	return times *pie *radius;
 }
+
+// 
