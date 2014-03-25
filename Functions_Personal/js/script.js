e69e3d4de60e73d79dbace08098b96179d3e56fe
Cliston Payne <Clistonp@yahoo.com>
2014-03-25 10:46:38 -0400

    if else

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,3 +2,12 @@
 
 //Diapers!
 var alphaExp = /^[0-99999]/;
+var diapersPerDay = prompt("How many Diapers do you use a day?");
+if (diapersPerDay==null || diapersPerDay==" ")
+{
+	alert("Please enter the number of diapers you use a day!");
+	location.reload(true);
+}
+else if (!diapersPerDay.matches(alphaExp)){
+
+}
