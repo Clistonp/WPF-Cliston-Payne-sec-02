0bd13eacf487c863142061aa5b0ea7fed8dba00b
Cliston Payne <Clistonp@yahoo.com>
2014-03-25 13:04:55 -0400

    added if statements

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,13 +1,23 @@
 //Cliston Payne Functions_Personal 3/25/14
 
 //Diapers!
-var alphaExp = /^[0-9]+$/;
+
 var diapersPerDay = (prompt("How many Diapers do you use a day?", ""));
 var daysInAWeek = (prompt("How many days are in a week?"));
-var diapersPerPack = (prompt("how many diapers are in one container you purchase?"))
+var diapersPerPack = (prompt("how many diapers are in one container you purchase?"));
 if (diapersPerDay==null || diapersPerDay==" ")
 {
 	alert("Please enter the number of diapers you use a day!");
 	location.reload(true);
 }	
+if (daysInAWeek==null || daysInAWeek==" ")
+{
+	alert("please enter the days per week!");
+	location.reload(true);
+}
+if (diapersPerPack==null || diapersPerPack==" ");
+{
+		alert("Please enter the number of diapers that come in a pack!");
+		location.reload(true);
+}
 
