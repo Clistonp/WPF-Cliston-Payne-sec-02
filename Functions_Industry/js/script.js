316ffbde743be3922cd6a5ee8783fe63ca647c12
Cliston Payne <Clistonp@yahoo.com>
2014-03-26 18:07:15 -0400

    stated my first variable and if statement

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,3 +2,12 @@
 
 //Functions_Industry
 //Sleeping Bag
+
+var outsideTemp = prompt("How cold is it outside?");
+while (outsideTemp==""){
+	alert("Please enter the temperature outside!");
+	outsideTemp = prompt("How cold is it outside?")
+}
+if (outsideTemp >= 0 && outsideTemp <= 40){
+	console.log("I hope you packed your green Sleeping Bag");
+}
