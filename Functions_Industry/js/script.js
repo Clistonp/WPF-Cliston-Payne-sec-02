8d463d7c0b0e823aaf08f1d748487cc42545151d
Cliston Payne <Clistonp@yahoo.com>
2014-03-26 18:27:55 -0400

    created else if statements

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -9,5 +9,12 @@ while (outsideTemp==""){
 	outsideTemp = prompt("How cold is it outside?")
 }
 if (outsideTemp >= 0 && outsideTemp <= 40){
-	console.log("I hope you packed your green Sleeping Bag");
+	console.log("I hope you packed your green Sleeping Bag!");
+}else if (outsideTemp >= 41 && outsideTemp <= 70){
+	console.log("Its still chilly outside go grab your black sleeping bag!")
+
+}else{
+	console.log("Sleep under the stars Marine!")
 }
+console.log()
+function calcSleepBagUse()
