e52e8a86ec15c18af1866499728f7c7c0150f82f
Cliston Payne <Clistonp@yahoo.com>
2014-03-26 23:27:07 -0400

    finished write up of code, testing now

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,12 +2,20 @@
 
 //Functionals_Wacky
 //Jelly Beans!
-
 alert("Thank you for stepping up to our game, theres only one rule grab a handful of jelly beans and count them out.")
 var jellyGrab = prompt("How many Jelly Beans did you grab?");
 while (jellyGrab==""){
 	alert("Please enter the amount of Jelly Beans you grabbed!");
 	jellyGrab = prompt("How many Jelly Beans did you grab?");
+		}
+
+var calcJellyInHand = function(jellyGrab){
+
+hand = (jellyGrab >= 30) ? "Great Job grab a prize off the top Shelf " : "OH, to bad try again when you become a man!";
+	return hand;
+
 }
-var hand = calcJellyInHand(jellyGrab);
-var jellyDunk;
+var hand; 
+var jellyDunk = calcJellyInHand();
+console.log(hand);
+
