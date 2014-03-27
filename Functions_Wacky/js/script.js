abcc60fcc4c13f1a2379ced31b22be1407346de4
Cliston Payne <Clistonp@yahoo.com>
2014-03-26 22:00:14 -0400

    creating my first alert and prompt for Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,4 +1,8 @@
 //Cliston Payne Functionals_Wacky 3/26/14 
 
 //Functionals_Wacky
-//
+//Jelly Beans!
+
+alert("Thank you for stepping up to our game, theres only one rule grab a handful of jelly beans and count them out.")
+var jellyGrab = ("How many Jelly Beans did you grab?");
+while (jellyGrab=="")
